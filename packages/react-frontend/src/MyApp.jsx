20b2c6de5152175a9d059db85f2bdd1aad4da311
Emily Rosales <erosal10@calpoly.edu>
2024-11-14 22:42:31 -0800

    Implement add character functionality and form submission handling

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -23,23 +23,6 @@ import Form from "./Form";
 //];
 function MyApp() {
   const [characters, setCharacters] = useState([
-    // {
-    //   name: "Charlie",
-    //   job: "Janitor"
-    // },
-    // {
-    //   name: "Mac",
-    //   job: "Bouncer"
-    // },
-    // {
-    //   name: "Dee",
-    //   job: "Aspring actress"
-    // },
-    // {
-    //   name: "Dennis",
-    //   job: "Bartender"
-    // }
-
 
   ]);
 
@@ -50,13 +33,17 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person){
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
-      <Form />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
